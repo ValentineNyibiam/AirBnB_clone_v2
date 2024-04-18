32ee75dde1952099d280d0816fbcc5ba0d1713d0
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-04-18 06:20:16 +0100

    fix(AirBnB_clone_v2): update queries in database setup files

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -2,6 +2,7 @@
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_db TO 'hbnb_test';
-GRANT SELECT PRIVILEGES ON perfomance_schema TO 'hbnb_test';
+GRANT ALL PRIVILEGES ON hbnb_test_db TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
+GRANT SELECT ON perfomance_schema TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;

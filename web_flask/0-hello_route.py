7ec241f2e07d2bc843b580fc7bc5c5e922a04c1a
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-06-12 00:04:52 +0100

    build(AirBnB:wf): update task 0 answer file

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" This is a script that starts a Flask application """
+""" This is a script that starts a Flask web application """
 from flask import Flask
 
 app = Flask(__name__)
